92672c691a020ef2fd4f2a1681fd59b7cf2e8f87
Todd VanGundy <todd@windsor-hotel.io>
2025-01-13 11:26:49 -0500

    Populate with source

---
+++ git-livereload-server/fcgiwrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+echo "Ensuring no stale socket exists..."
+rm -f /var/run/fcgiwrap.socket
+
+# Start fcgiwrap
+fcgiwrap -s unix:/var/run/fcgiwrap.socket &
+
+# Wait for the socket to be created
+while [ ! -S /var/run/fcgiwrap.socket ]; do
+  sleep 0.1
+done
+
+# Set the permissions
+chown nginx:nginx /var/run/fcgiwrap.socket
+chmod 0660 /var/run/fcgiwrap.socket
+
+# Keep the script running to not exit and hence keep the service running
+wait
